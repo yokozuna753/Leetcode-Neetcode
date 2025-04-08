4bd04bf9bb889af9d904beb8e0aa4f2cb4ded56a
yokozuna753 <brian894526@gmail.com>
2025-04-08 18:44:16 -0400

    finished two-sum

---
+++ two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(nums: list[int], target: int) -> list[int]:
+        hash_table = {}
+
+        for i in range(len(nums)):
+            res_num = target - nums[i]
+            if nums[i] in hash_table:
+                return [hash_table[nums[i]], i]
+            hash_table[res_num] = i
+
+print(Solution.twoSum(nums = [3,4,5,6], target = 7))
