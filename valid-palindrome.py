3fc38306f5a62e68d7e48a9f1f7fa7611200677d
yokozuna753 <brian894526@gmail.com>
2025-04-11 20:00:54 -0400

    solution 2 for valid palindrome using 2 pointers

--- valid-palindrome.py
+++ valid-palindrome.py
@@ -18,10 +18,32 @@ Output: false
 # spit the string on the spaces
 
 
-class Solution:
+class Solution1:
     def isPalindrome(s: str) -> bool:
         filtered = ''.join([c.lower() for c in s if c.isalnum()])
         return filtered == filtered[::-1]
 
 
-Solution.isPalindrome(s="Was it a car or a cat I saw?")
+Solution1.isPalindrome(s="Was it a car or a cat I saw?")
+
+
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def isAlphaNum(c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <=ord(c) <= ord('9')) 
+    
+
+# print(Solution2.isAlphaNum())
