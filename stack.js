e68636f9cd43113cdfcc0e6c75e9128d537a5b2c
yokozuna753 <brian894526@gmail.com>
2025-04-27 21:35:06 -0400

    stack problem

---
+++ stack.js
@@ -0,0 +1,66 @@
+/*
+
+Valid Parentheses
+
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+
+Example 1:
+
+Input: s = "[]"
+
+Output: true
+Example 2:
+
+Input: s = "([{}])"
+
+Output: true
+Example 3:
+
+Input: s = "[(])"
+
+Output: false
+Explanation: The brackets are not closed in the correct order.
+
+Constraints:
+
+1 <= s.length <= 1000
+
+([{}])
+! [")", "]", "{"]
+*/
+// split "s" into array
+
+const isValid = (s) => {
+  let i = 0;
+  const hashMap = {
+    "}": "{",
+    "]": "[",
+    ")": "(",
+  };
+  if (s[0] in hashMap) {
+    return false;
+  }
+  const stack = [];
+  while (i < s.length) {
+    if (!(s[i] in hashMap)) {
+      stack.push(s[i]);
+    } else {
+      if (!(hashMap[s[i]] === stack.pop())) {
+        return false;
+      }
+    }
+    i++;
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid("([{}])"));
+console.log(isValid("(){}[]"));
+console.log(isValid("[(])"));
