b9d853a4336345397675f291b0e13d08b5b6d3a6
yokozuna753 <brian894526@gmail.com>
2025-05-29 20:54:51 -0400

    container with most water

--- 3sum.py
+++ 3sum.py
@@ -16,8 +16,7 @@ class Solution:
                     while l < r and nums[l] == nums[l + 1]:
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
-                        r -= 1
-                        
+                        r -= 1  
                     l += 1
                     r -= 1
                 elif total < 0:
