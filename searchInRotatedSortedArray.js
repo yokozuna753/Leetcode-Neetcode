ae4b3d9fd1026b66ddffdbe646336834fc11b670
yokozuna753 <brian894526@gmail.com>
2025-06-23 10:21:09 -0400

    find target in sorted rotated array; binary search

---
+++ searchInRotatedSortedArray.js
@@ -0,0 +1,56 @@
+/*
+
+You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+[3,4,5,6,1,2] if it was rotated 4 times.
+[1,2,3,4,5,6] if it was rotated 6 times.
+Given the rotated sorted array nums and an integer target, return the index of target within nums, or -1 if it is not present.
+
+You may assume all elements in the sorted rotated array nums are unique,
+
+A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+Example 1:
+
+Input: nums = [3,4,5,6,1,2], target = 1
+
+Output: 4
+Example 2:
+
+Input: nums = [3,5,6,0,1,2], target = 4
+
+Output: -1
+
+*/
+
+function search(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] === target) return mid;
+
+        // Determine which half is sorted
+        if (nums[left] <= nums[mid]) {
+            // Left half is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // Right half is sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(search([1,2,3,4,5,6], target=5));
+console.log(search(nums = [3,4,5,6,1,2], target = 10));
+console.log(search(nums = [5,1,2,3,4], target = 1));
