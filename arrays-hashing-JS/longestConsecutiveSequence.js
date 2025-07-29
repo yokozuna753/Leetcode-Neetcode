7a73535968c40cb84a05ac46b1798aefc52c93e3
yokozuna753 <brian894526@gmail.com>
2025-07-28 21:51:19 -0400

    moved everything to the if statement

--- arrays-hashing-JS/longestConsecutiveSequence.js
+++ arrays-hashing-JS/longestConsecutiveSequence.js
@@ -20,17 +20,19 @@ Output: 7
 */
 
 function longestConsecutive(nums) {
-    let set = new Set(nums);
-    let longest = 0;
-    for (let num of set) {
-        if (!set.has(num - 1));
-        let length = 1;
-        while (set.has(num + length)) {
-            length++;
-        }
-        longest = Math.max(longest, length);
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let length = 1;
+      while (numSet.has(num + length)) {
+        length++;
+      }
+      longest = Math.max(longest, length);
     }
-    return longest;
+  }
+  return longest;
 }
 
-console.log(longestConsecutive([2,20,4,10,3,4,5]));
+console.log(longestConsecutive([2, 20, 4, 10, 3, 4, 5]));
