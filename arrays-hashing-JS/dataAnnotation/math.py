2fc803c322971093576321e0faa13abd3303a6ca
yokozuna753 <brian894526@gmail.com>
2025-07-29 12:27:31 -0400

    interesting staircase problem using python

--- arrays-hashing-JS/dataAnnotation/math.py
+++ arrays-hashing-JS/dataAnnotation/math.py
@@ -9,4 +9,31 @@ def extract_remove_last_digit(n) -> None:
         print(f'New N: {n}\n')
 
 
-extract_remove_last_digit(1233450)
+
+''' 
+step = 3
+subsets = [[1],[2,3]]
+[4,5,6] -> len = 3
+
+
+'''
+def create_staircase(nums):
+  step = 1
+  subsets = []
+  while len(nums) != 0:
+    if len(nums) >= step: # if the length of input nums >= step
+      subsets.append(nums[0:step])
+      nums = nums[step:]
+      step += 1
+    else:
+      return False
+      
+  return subsets
+
+
+
+print(create_staircase([1,2,3,4,5,6]))
+nums = [1,2,3,4,5,6]
+print(nums[0:2])
+
+
