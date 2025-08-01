9819a9dc87e15e6d048c6ade2fc2d931a29da5ee
yokozuna753 <brian894526@gmail.com>
2025-08-01 17:23:56 -0400

    added solution with using a single array and adding the nodes to it

--- linkedList/reorderList.js
+++ linkedList/reorderList.js
@@ -8,8 +8,7 @@
  * }
  */
 
-
-  /**
+/**
   //  * @param {ListNode} head
   //  * @return {void}
    *
@@ -27,77 +26,104 @@
    * - return the new linked list
    */
 
+// class ListNode {
+//   constructor(val = 0, next = null) {
+//     this.val = val;
+//     this.next = next;
+//   }
+// }
 
-class ListNode {
-  constructor(val = 0, next = null) {
-    this.val = val;
-    this.next = next;
+// class LinkedList {
+//   constructor(head = null) {
+//     this.head = head;
+//   }
+//   append(val) {
+//     let newNode = new ListNode(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       return;
+//     }
+//     let curr = this.head;
+//     while (curr.next) {
+//       curr = curr.next;
+//     }
+//     curr.next = newNode;
+//   }
+
+//   reorderList(head) {
+//     let tempArray = [];
+//     let curr = head;
+//     while (curr) {
+//       tempArray.push(curr.val);
+//       curr = curr.next;
+//     }
+//     // [ 2, 4, 6, 8 ]
+//     let mid = Math.ceil(tempArray.length / 2);
+
+//     let half1 = tempArray.slice(0, mid);
+//     let half2 = tempArray.slice(mid).reverse();
+//     let res = new LinkedList();
+//     let first = 0,
+//       second = 0;
+
+//     while (first < half1.length || second < half2.length) {
+//   if (first < half1.length) {
+//     res.append(half1[first]);
+//     first++;
+//   }
+//   if (second < half2.length) {
+//     res.append(half2[second]);
+//     second++;
+//   }
+//     }
+
+//     return res.head;
+//   }
+// }
+
+// let testArr = [2, 4, 6, 8, 10];
+// let newList = new LinkedList();
+
+// for (let index = 0; index < testArr.length; index++) {
+//   const val = testArr[index];
+//   newList.append(val);
+// }
+
+// const reOrderedHead = newList.reorderList(newList.head);
+
+// let curr = reOrderedHead;
+
+// while (curr) {
+//   console.log(curr.val);
+//   curr = curr.next;
+// }
+
+function reorderList(head) {
+  let nodes = [];
+  let curr = head;
+  while (curr) {
+    nodes.push(curr);
+    curr = curr.next;
   }
-}
+  /* [ 2, 4, 6, 8 ]
+           [ 2, 4, 6, 8, 10 ]
+                   i
+                   j
+            2->10->4->8->6
+            add node at i
+            add node at j to i's next
+        // let mid = Math.ceil(tempArray.length / 2);
+        */
 
-class LinkedList {
-  constructor(head = null) {
-    this.head = head;
+  let i = 0,
+    j = nodes.length - 1;
+
+  while (i < j) {
+    nodes[i].next = nodes[j];
+    i++;
+    nodes[j].next = nodes[i];
+    j--;
   }
-  append(val) {
-    let newNode = new ListNode(val);
-    if (!this.head) {
-      this.head = newNode;
-      return;
-    }
-    let curr = this.head;
-    while (curr.next) {
-      curr = curr.next;
-    }
-    curr.next = newNode;
-  }
 
-  reorderList(head) {
-    let tempArray = [];
-    let curr = head;
-    while (curr) {
-      tempArray.push(curr.val);
-      curr = curr.next;
-    }
-    // [ 2, 4, 6, 8 ]
-    let mid = Math.ceil(tempArray.length / 2);
-
-    let half1 = tempArray.slice(0, mid);
-    let half2 = tempArray.slice(mid).reverse();
-    let res = new LinkedList();
-    let first = 0,
-      second = 0;
-
-    while (first < half1.length || second < half2.length) {
-  if (first < half1.length) {
-    res.append(half1[first]);
-    first++;
-  }
-  if (second < half2.length) {
-    res.append(half2[second]);
-    second++;
-  }
-    }
-
-
-    return res.head;
-  }
-}
-
-let testArr = [2, 4, 6, 8, 10];
-let newList = new LinkedList();
-
-for (let index = 0; index < testArr.length; index++) {
-  const val = testArr[index];
-  newList.append(val);
-}
-
-
-const reOrderedHead = newList.reorderList(newList.head);
-
-let curr = reOrderedHead;
-
-while (curr) {
-  console.log(curr.val);
-  curr = curr.next;
+  nodes[i].next = null;
 }
